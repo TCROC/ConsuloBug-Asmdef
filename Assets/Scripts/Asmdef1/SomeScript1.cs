6793b846210c88606446b511932c95feaa3537b4
Travis Lange <travislange12@gmail.com>
2020-12-05 13:22:13 -0500

    First commit

---
+++ Assets/Scripts/Asmdef1/SomeScript1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeScript1 : MonoBehaviour
+{
+    public SomeScript2 someScript2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
